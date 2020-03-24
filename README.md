714d30d5e4802f3dd13ae327cd30f3b5629997ca
Max Hutchison <mhutchi2517@gmail.com>
2020-03-23 19:10:42 -0700

    It finally works!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # week3_homework
+
+This webpage will let you choose a password length and characters and them generate a random password for you.
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 const strings = {
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowercase: "abcdefghijklmnopqrstuvwxyz",
@@ -7,86 +8,90 @@ const strings = {
   special: "!@#$%^&*"
 };
 
-// Write password to the #password input
-function writePassword() {
-  
-  var charLowercase = confirm("Would you like to use lowercase characters?");
+var passwordLength = prompt("Choose a password length between 1-128 characters.");
+alert("You picked " + passwordLength + " characters.");
+var charLowercase = confirm("Would you like to use lowercase characters?");
+if (charLowercase === true) {
+  alert("You want to include lowercase characters.");
+} else {
+  alert("You do not want to include lowercase characters.");
+};
+var charUppercase = confirm("Would you like to use uppercase characters?")
+if (charUppercase === true) {
+  alert("You want to include uppercase characters.");
+} else {
+  alert("You do not want to include uppercase characters.");
+};
+var charNumeric = confirm("Would you like to use numeric characters?");
+if (charNumeric === true) {
+  alert("You do want to include numeric characters.");
+} else {
+  alert("You do not want to include numeric characters.");
+};
+var charSpecial = confirm("Would you like to use special characters?");
+if (charSpecial === true) {
+  alert("You do want to include special characters.");
+} else {
+  alert("You do not want to include special characters.");
+};
+
+
+
+console.log(charSpecial, charUppercase, charLowercase, charNumeric, passwordLength);
+
+
+function generatePassword() {
+  var amount = parseInt(passwordLength);
+  var choices = [];
+
+
+  console.log(amount);
+
+
+
+
   if (charLowercase === true) {
-    alert("You want to include lowercase characters.");
-  } else {
-    alert("You do not want to include lowercase characters.");
+    choices.push(strings["lowercase"]);
   };
-  var charUppercase = confirm("Would you like to use uppercase characters?")
   if (charUppercase === true) {
-    alert("You want to include uppercase characters.");
-  } else {
-    alert("You do not want to include uppercase characters.");
+    choices.push(strings["uppercase"])
   };
-  var charNumeric = confirm("Would you like to use numeric characters?");
-  if (charNumeric === true) {
-    alert("You do want to include numeric characters.");
-  } else {
-    alert("You do not want to include numeric characters.");
-  };
-  var charSpecial = confirm("Would you like to use special characters?");
   if (charSpecial === true) {
-    alert("You do want to include special characters.");
-  } else {
-    alert("You do not want to include special characters.");
+    choices.push(strings["special"])
   };
-  var passwordLength = prompt("Choose a password length between 1-128 characters.");
-  alert("You picked " + passwordLength + " characters.");
-  console.log(charSpecial);
-  console.log(charUppercase);
-  console.log(charLowercase);
-  console.log(charNumeric);
+  if (charNumeric === true) {
+    choices.push(strings["number"])
+  };
+
+  var choiceString = choices.join('');
+  var choiceAmount = choiceString.length;
+  console.log(choiceString);
+  var passcode = [];
+  for (j = 0; j < amount; j++) {
+    var char = choiceString.charAt(Math.floor(Math.random() * choiceAmount));
+    passcode.push(char);
+
+    
+  }
   
-  
+  var passcodeString = passcode.join('');
+  return passcodeString;
+
+  console.log(char);
+}
+// Write password to the #password input  
+function writePassword() {
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  console.log(password)
 
 };
 
-function generatePassword(lower, upper, num, spec) {
-  
-  let main = "";
-  let pass = "";
-  
-
-  const choices = {
-    lowercase: lower,
-    uppercase: upper, 
-    number: num,
-    special: spec
-  };
-
-  for (i = 0; i < passwordLength; i++) {
-    main += (object.values(choices)[i]) ? strings[object.keys(choices)[i]] : "";
-    }
-    if (main != "" && length > 0) {
-      for (i = 0; i < length; i++) {
-        pass += main[Math.floor(Math.random() * main.length)];
-      }
-      console.log(pass)
-     
-
-   
-
-    }
-   
 
 
-  
-
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// Questions and Prompts for confirmations
-
